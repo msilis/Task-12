474ed2de7449d74bcc50c7d62924f241d416f76d
msilis <mikssilis@gmail.com>
2022-12-12 22:59:55 +0000

    updated logic

--- wordManipulation.js
+++ wordManipulation.js
@@ -4,19 +4,41 @@ function clickHandler(){
     let inputWord = document.getElementById('word-input').value;
 
     //Every second letter is !
-    
-    for (let i = 1; i < inputWord.length; i+=2){
-        console.log(inputWord.replace('e', '!'));
+    let splitWord = inputWord.split("");
+    let manipulatedWord = splitWord;
+    console.log(splitWord);
+    for (let i = 1; i < manipulatedWord.length; i+=2){
+        manipulatedWord.splice(i, 1, '!');
     }
-    console.log('button clicked');
-    console.log(inputWord);
+    let joinedWord = splitWord.join('');
+    console.log(joinedWord)
+    document.getElementById('bullet1').innerHTML = `1. ${inputWord} ------> ${joinedWord}`
     
-
     //Word is reversed
 
+    let reversedWordArray = inputWord.split('').reverse();
+    let reversedWord = reversedWordArray.join('');
+    console.log(reversedWord);
+    document.getElementById('bullet2').innerHTML = `2. ${inputWord} ------> ${reversedWord}`
+
     //Every 6th letter is uppercaase
+    let upperCaseWord = inputWord.split("");
+     for (let i=5; i<upperCaseWord.length; i+=6){
+        upperCaseWord[i] = upperCaseWord[i].toUpperCase();
+     }
+    console.log(upperCaseWord.join(''))
+    document.getElementById('bullet3').innerHTML = `3. ${inputWord} ------> ${upperCaseWord.join('')}`
+
 
     //Array of ASCII values
+
+    let charCodeWord = inputWord.split("");
+    for (let i=0; i<charCodeWord.length; i++){
+        charCodeWord[i] = charCodeWord[i].charCodeAt();
+        
+    }
+    console.log(charCodeWord);
+    document.getElementById('bullet4').innerHTML = `3. ${inputWord} ------> ${charCodeWord}`
 
 }
 
